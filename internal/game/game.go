18d17266677f1f7bdd8e6cd55bfcec4dc1412c90
Nathaniel Wheeler <nathan@mailftp.com>
2021-01-28 15:03:07 -0700

    go, look, rooms implemented

--- internal/game/game.go
+++ internal/game/game.go
@@ -1,20 +1,54 @@
 package game
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Game is the overall structure, holding entities, locations, methods, etc.
-type Game struct{}
+type Game struct {
+	CurrentRoomID uint
+	Rooms         map[uint]Room
+}
 
 // NewGame will construct a game
 func NewGame() (*Game, error) {
-	g := Game{}
+	g := Game{CurrentRoomID: uint(1)}
+	g.GenerateRooms()
 
-	fmt.Println("You are in a glistening mine.  There is an exit to the North.")
+  fmt.Println(g.GetRoomDescription(g.CurrentRoomID))
 
 	return &g, nil
 }
 
 // Execute takes in a command, passes it to internal logic, and returns whatever the response is.
 func (g *Game) Execute(command string) (string, error) {
-  return fmt.Sprintf("You just input '%s'", command), nil
+	// TODO Put a proper validation layer in?
+	if command == "" {
+		return "Type something in, yo.", nil
+	}
+	command = strings.ToLower(command) // normalize to lowercase
+
+	// Turn command into fields
+	fields := strings.Fields(command)
+	// args := fields[:2]
+
+	switch fields[0] {
+	case "go":
+		// TODO refactor for simplicity, this is unwieldy in the long run
+    // I could use util.StringExistsInSlice on the location directions, keeping my switch statements a bit more maintainable
+    if len(fields) < 2 {
+      return "Go where?", nil
+    }
+    return g.GoDirection(fields[1]), nil
+  case "look":
+    fmt.Println("Got here")
+
+    desc := g.GetRoomDescription(g.CurrentRoomID)
+		return desc, nil
+	case "something":
+		return "Don't you sass me.", nil
+	default:
+		return fmt.Sprintf("'%s' does not compute.", command), nil
+	}
 }
---
+++ internal/game/room.go
@@ -0,0 +1,64 @@
+package game
+
+// Room holds the directions and entities within the room.
+type Room struct {
+	Name        string
+	Description string
+	Doors       []Door
+	// Items []Item
+}
+
+// Door holds the metadata required to journey to a different room.
+type Door struct {
+	RoomID    uint
+	Direction string
+	// IsLocked bool
+	// IsVisible bool
+	// GoDesc string
+	// LookDesc string
+}
+
+// GenerateRooms will add a list of rooms to the game
+func (g *Game) GenerateRooms() {
+	g.Rooms = map[uint]Room{
+		1: {
+			Name:        "Glistening Mine",
+			Description: "You are in a glistening mine.  There is an exit to the NORTH.",
+			Doors: []Door{
+				Door{RoomID: 2, Direction: "north"},
+			},
+		},
+		2: {
+			Name:        "Mineshaft",
+			Description: "The tunnel ends in a mineshaft.  You can go SOUTH or DOWN.",
+			Doors: []Door{
+				Door{RoomID: 1, Direction: "south"},
+				Door{RoomID: 3, Direction: "down"},
+			},
+		},
+		3: {
+			Name:        "Underground Lake",
+			Description: "You are next to an underground lake.  Looks like you're stuck here!",
+		},
+	}
+}
+
+// GetRoomDescription will return the room's description given an id
+func (g *Game) GetRoomDescription(id uint) string {
+	description := g.Rooms[id].Description
+	return description
+}
+
+// GoDirection will check if the direction is valid given the room, change the game's CurrentRoomID, and return the new room's description.
+func (g *Game) GoDirection(dir string) string {
+	room := g.Rooms[g.CurrentRoomID]
+
+	for _, door := range room.Doors {
+		if dir == door.Direction {
+			g.CurrentRoomID = door.RoomID
+			return g.GetRoomDescription(g.CurrentRoomID)
+		}
+	}
+
+	return "You can't go that way."
+}
